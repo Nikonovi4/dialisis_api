ca54c0d4b66adb72c5c11db6df8370c8b95d68a6
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-11-22 22:38:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+// Слушаем 3000 порт
+
+const app = express();
+const routes = require("./routes/index");
+
+const { PORT = "3000", DB_URL = "mongodb://0.0.0.0/dialisisdb" } = process.env;
+
+mongoose
+  .connect(DB_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("connected to bd");
+  });
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`server is runing on port ${PORT}`);
+});
